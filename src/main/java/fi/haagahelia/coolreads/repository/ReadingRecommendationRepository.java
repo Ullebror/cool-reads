779193d90a6f414e5e2a9a5da9b0d732ca415d6e
Olli Salo <ullebror@gmail.com>
2023-11-30 23:43:07 +0200

    Added a way to get all recommendations by categoryId

--- src/main/java/fi/haagahelia/coolreads/repository/ReadingRecommendationRepository.java
+++ src/main/java/fi/haagahelia/coolreads/repository/ReadingRecommendationRepository.java
@@ -2,10 +2,13 @@ package fi.haagahelia.coolreads.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import fi.haagahelia.coolreads.model.Recommendation;
 import java.util.List;
 
 @Repository
 public interface ReadingRecommendationRepository extends JpaRepository<Recommendation, Long> {
 	List<Recommendation> findAllByOrderByCreationDateDesc();
+
+	List<Recommendation> findByCategoryIdOrderByCreationDateDesc(Long id);
 }
