29547a3eeacc838f148c09213d1386979a2aebbe
Olli Salo <ullebror@gmail.com>
2023-12-02 12:18:31 +0200

    Added recommendationList folder and RecommendationList.jsx. Changed the template recommendationlist.html to work with these

---
+++ frontend/recommendationList/renderRecommendationList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("recommendationListRoot"));
+root.render(<h1>Reading recommendation list coming soon!</h1>);
