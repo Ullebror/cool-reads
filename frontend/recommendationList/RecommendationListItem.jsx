ba8a8df50d3359a67fa0e56f8312f454711d0f89
Olli Salo <ullebror@gmail.com>
2023-12-03 10:20:50 +0200

    Formatted the date

--- frontend/recommendationList/RecommendationListItem.jsx
+++ frontend/recommendationList/RecommendationListItem.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 export default function RecommendationListItem(props) {
+  const date = new Date(props.recommendation.creationDate);
+  const formattedDate = date.toLocaleDateString('fi-FI', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
+
   return (
     <tr key={props.recommendation.id}>
         <td>{props.recommendation.title}</td>
         <td><a href={props.recommendation.link}>{props.recommendation.link}</a></td>
         <td>{props.recommendation.description}</td>
-        <td>{props.recommendation.creationDate}</td>
+        <td>{formattedDate}</td>
         <td>{props.recommendation.category.name}</td>
         <td> <form method="GET" action={`/edit/${props.recommendation.id}`}>
             <button className="btn btn-primary btn-xs">Edit</button>
