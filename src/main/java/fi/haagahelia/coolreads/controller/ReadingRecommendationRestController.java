c4ff35f643223f43a1be33802695d841a8979cf3
Edoardo Vacca <edoardo.vacca@hotmail.it>
2023-12-04 18:47:21 +0200

    added frontend maven plugin

--- src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationRestController.java
+++ src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationRestController.java
@@ -3,6 +3,7 @@ package fi.haagahelia.coolreads.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/recommendations")
+@CrossOrigin(origins = "https://localhost:5173")
 @Tag(name="Recommendations", description="Information about all the reading recommendations")
 public class ReadingRecommendationRestController {
 	@Autowired
