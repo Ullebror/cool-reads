321137ee7c2872a84bd15ad5f429978a2d24cdf4
Olli <ullebror@gmail.com>
2023-12-08 13:37:08 +0200

    Added Security Configuration

---
+++ src/main/java/fi/haagahelia/coolreads/SecurityConfig.java
@@ -0,0 +1,32 @@
+package fi.haagahelia.coolreads;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers(antMatcher("/"), antMatcher("/register"),
+                                antMatcher("/categorylist"), antMatcher("/frontend/**"), antMatcher("/api/**"), antMatcher("/error")).permitAll().anyRequest()
+                                .authenticated());
+
+        http.formLogin((form) -> form.permitAll());
+        http.logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+}
