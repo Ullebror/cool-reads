2b4ca5c8c6348112930f9fbabe482a945c855a9c
Olli <ullebror@gmail.com>
2023-12-13 18:32:56 +0200

    Created swagger documentation for the endpoints.

--- src/main/java/fi/haagahelia/coolreads/controller/AppUserRestController.java
+++ src/main/java/fi/haagahelia/coolreads/controller/AppUserRestController.java
@@ -13,18 +13,23 @@ import org.springframework.web.server.ResponseStatusException;
 
 import fi.haagahelia.coolreads.model.AppUser;
 import fi.haagahelia.coolreads.repository.AppUserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name="Users", description="Information about the users of Cool Reads")
 public class AppUserRestController {
 	@Autowired
 	private AppUserRepository userRepository;
 
+	@Operation(summary = "Users", description = "Fetches all the users")
 	@GetMapping("")
 	public List<AppUser> getAllUsers() {
 		return userRepository.findAll();
 	}
 
+	@Operation(summary = "Current user", description = "Fetches the current user but only shows it if logged in.")
 	@GetMapping("/current")
 	public AppUser getCurrentUser(@AuthenticationPrincipal UserDetails userDetails) {
 		if (userDetails == null) {
