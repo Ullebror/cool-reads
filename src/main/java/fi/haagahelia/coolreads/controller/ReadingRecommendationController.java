d5184584460f165c1853d59b7cef433f2953786e
Olli <ullebror@gmail.com>
2023-12-15 15:00:14 +0200

    Made modifications to the Recommendation Controller to add user in recommendations

--- src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationController.java
+++ src/main/java/fi/haagahelia/coolreads/controller/ReadingRecommendationController.java
@@ -3,8 +3,11 @@ package fi.haagahelia.coolreads.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +15,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.haagahelia.coolreads.dto.RecommendationDto;
+import fi.haagahelia.coolreads.model.AppUser;
 import fi.haagahelia.coolreads.model.Recommendation;
 import fi.haagahelia.coolreads.repository.ReadingRecommendationRepository;
+import jakarta.validation.Valid;
+import fi.haagahelia.coolreads.repository.AppUserRepository;
 import fi.haagahelia.coolreads.repository.CategoryRepository;
 
 //import java.time.LocalDateTime;
@@ -25,13 +32,15 @@ public class ReadingRecommendationController {
 	private ReadingRecommendationRepository readingRepository;
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private AppUserRepository userRepository;
 
 	private static final Logger log = LoggerFactory.getLogger(ReadingRecommendationController.class);
 
 	// Add new recommendation
 	@GetMapping("/add")
 	public String addRecommendation(Model model) {
-		model.addAttribute("recommendation", new Recommendation());
+		model.addAttribute("recommendation", new RecommendationDto());
 		model.addAttribute("categories", categoryRepository.findAll());
 		log.info("form");
 		return "addrecommendation";
@@ -39,8 +48,21 @@ public class ReadingRecommendationController {
 
 	// Save new recommendation
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String saveRecommendation(@ModelAttribute("recommendation") Recommendation recommendation) {
-		readingRepository.save(recommendation);
+	public String saveRecommendation(@Valid @ModelAttribute("recommendation") RecommendationDto recommendation, BindingResult bindingResult,
+				@AuthenticationPrincipal UserDetails userDetails, Model model) {
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("recommendation", recommendation);
+			return "addrecommendation";
+		}
+		
+		AppUser user = userRepository.findByUsername(userDetails.getUsername());
+		
+		if (user == null) {
+			return "redirect:/";
+		}
+		Recommendation newRecommendation = new Recommendation(recommendation.getTitle(), recommendation.getLink(), recommendation.getDescription(),
+				recommendation.getCategory(), user);
+		readingRepository.save(newRecommendation);
 		return "redirect:/";
 	}
 
